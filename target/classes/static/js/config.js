996cbd06a58dbc4f26b49832d1780170e97d9778
Emre Can <emrecan19oo7@gmail.com>
2025-07-23 17:59:19 +0300

    new package

--- target/classes/static/js/config.js
+++ target/classes/static/js/config.js
@@ -1,3 +1,5 @@
-if (typeof API_BASE_URL == 'undefined') {
-	var API_BASE_URL = "http://192.168.1.80:8080/api";
+if (typeof API_BASE_URL === 'undefined') {
+  var API_BASE_URL = window.location.hostname === 'localhost' || window.location.hostname.startsWith('192.168.')
+    ? "http://192.168.1.80:8080/api"
+    : "https://film-onersene.onrender.com/api";
 }
